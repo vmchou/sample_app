9151274b87c45f2df4b2a0b0ba1cad9a61fd59a6
Victor Chou <victorchou@ucla.edu>
2015-04-10 17:06:20 -0700

    Fixing indentations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-					 password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
   
   test "should be valid" do
@@ -15,12 +15,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
-   test "email should be present" do
+  test "email should be present" do
     @user.email = "     "
     assert_not @user.valid?
   end
   
-   test "name should not be too long" do
+  test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
   end
